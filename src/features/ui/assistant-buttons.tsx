ecffab88585b2dd464f007c41a2cb71fca9896e5
Fred <frederic.delage@chde.qld.gov.au>
2024-05-21 12:33:40 +1000

    WIP - demo hacking

--- src/features/ui/assistant-buttons.tsx
+++ src/features/ui/assistant-buttons.tsx
@@ -1,7 +1,6 @@
 "use client"
 import * as Tooltip from "@radix-ui/react-tooltip"
-import { CheckIcon, ClipboardIcon, ThumbsUp, ThumbsDown, BookOpenText } from "lucide-react"
-// import { CheckIcon, ClipboardIcon, ThumbsUp, ThumbsDown, BookOpenText, Sparkle, Sparkles } from "lucide-react"
+import { CheckIcon, ClipboardIcon, ThumbsUp, ThumbsDown, BookOpenText, Sparkle, Sparkles } from "lucide-react"
 import React from "react"
 
 import Typography from "@/components/typography"
@@ -10,14 +9,15 @@ import { TooltipProvider } from "@/features/ui/tooltip-provider"
 import { useWindowSize } from "@/features/ui/windowsize"
 
 interface AssistantButtonsProps {
+  fleshScore: number
   isIconChecked: boolean
   thumbsUpClicked: boolean
   thumbsDownClicked: boolean
-  // rewriteWithSuggestions: boolean
+  rewriteWithSuggestions: boolean
   handleCopyButton: () => void
   handleThumbsUpClick: () => void
   handleThumbsDownClick: () => void
-  // handleRewriteWithSuggestions: () => void
+  handleRewriteWithSuggestions: () => void
 }
 
 interface FleschButtonProps {
@@ -40,7 +40,7 @@ const useButtonStyles = (): { iconSize: number; buttonClass: string } => {
   return { iconSize, buttonClass }
 }
 
-export const FleschButton: React.FC<FleschButtonProps> = ({ fleschScore }) => {
+const FleschButton: React.FC<FleschButtonProps> = ({ fleschScore }) => {
   const { iconSize } = useButtonStyles()
 
   return (
@@ -53,7 +53,10 @@ export const FleschButton: React.FC<FleschButtonProps> = ({ fleschScore }) => {
               {fleschScore}
             </div>
           </Tooltip.Trigger>
-          <Tooltip.Content side="top" className="z-20 rounded-md bg-primary-foreground p-4 text-foreground shadow-lg">
+          <Tooltip.Content
+            side="top"
+            className="z-20 rounded-md bg-primary-foreground px-4 py-2 text-foreground shadow-lg"
+          >
             <Typography variant="p">
               <strong>Flesch-Kincaid Score (KFKS):</strong> The Flesch-Kincaid Score below shows
               <br />
@@ -72,18 +75,20 @@ export const FleschButton: React.FC<FleschButtonProps> = ({ fleschScore }) => {
 }
 
 export const AssistantButtons: React.FC<AssistantButtonsProps> = ({
+  fleshScore,
   isIconChecked,
   thumbsUpClicked,
   thumbsDownClicked,
-  // rewriteWithSuggestions,
+  rewriteWithSuggestions,
   handleCopyButton,
   handleThumbsUpClick,
   handleThumbsDownClick,
-  // handleRewriteWithSuggestions,
+  handleRewriteWithSuggestions,
 }) => {
   const { iconSize, buttonClass } = useButtonStyles()
+
   return (
-    <div className="flex w-full gap-4">
+    <div className="flex w-full gap-2">
       <Button
         ariaLabel="Copy text"
         variant={"ghost"}
@@ -116,16 +121,19 @@ export const AssistantButtons: React.FC<AssistantButtonsProps> = ({
       >
         {thumbsDownClicked ? <CheckIcon size={iconSize} /> : <ThumbsDown size={iconSize} />}
       </Button>
-      {/* <Button
-        variant={"positive"}
+
+      <Button
+        ariaLabel="Rewrite with suggestions"
+        variant={"ghost"}
         size={"default"}
         className={buttonClass}
         title="Rewrite with suggestions"
         onClick={handleRewriteWithSuggestions}
-        ariaLabel="Rewrite with suggestions"
       >
         {rewriteWithSuggestions ? <Sparkles size={iconSize} /> : <Sparkle size={iconSize} />}
-      </Button> */}
+      </Button>
+
+      <FleschButton fleschScore={fleshScore} />
     </div>
   )
 }
---
+++ src/features/ui/rewriteMessageModal.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from "react"
+
+import Typography from "@/components/typography"
+
+import { Button } from "./button"
+
+interface ModalProps {
+  chatThreadId: string
+  chatMessageId: string
+  rewriteAction: "Simplify" | "Improve" | "Explain"
+  message: string
+  open: boolean
+  onClose: () => void
+  onSubmit: () => void
+}
+
+export default function RewriteMessageModal(props: ModalProps): ReturnType<FC> {
+  return (
+    <div
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="rewriteMessageHeading"
+      className={`fixed inset-0 z-90 flex items-center justify-center bg-black/50 ${props.open ? "block" : "hidden"}`}
+    >
+      <div className="z-90 mx-auto w-full max-w-lg overflow-hidden rounded-lg bg-background p-4">
+        <div className="mb-4">
+          <Typography id="rewriteMessageHeading" variant="h4">
+            {props.rewriteAction} message
+          </Typography>
+        </div>
+        <div className="mb-4">
+          <textarea
+            name={props.chatMessageId + "Rewrite text"}
+            id={props.chatMessageId + "Rewrite text id"}
+            aria-label="Enter your feedback"
+            placeholder=""
+            rows={15}
+            className="flex min-h-[80px] w-full rounded-md border border-gray-300 border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            value={rewriteTexts(props.rewriteAction, props.message)}
+            // onChange={event => props.onFeedbackReasonChange(event.target.value)}
+          />
+        </div>
+
+        {/* <FeedbackButtons
+          selectedType={props.feedbackType ?? FeedbackType.None}
+          onFeedbackTypeChange={props.onFeedbackTypeChange}
+        /> */}
+        <div className="mt-4 flex justify-center gap-2">
+          <Button variant="default" onClick={props.onSubmit} disabled={false}>
+            {props.rewriteAction}
+          </Button>
+          <Button variant="destructive" onClick={props.onClose}>
+            Close
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const rewriteTexts = (action: ModalProps["rewriteAction"], message: string): string => {
+  switch (action) {
+    case "Simplify":
+      return `Simplify the below, consider length, readability and tone of voice:\n\n===Text to simplify===\n${message}\n===End of text to simplify===`
+    case "Improve":
+      return `Improve the below, consider inclusive language, length, readability and tone of voice:\n\n===Text to improve===\n${message}\n===End of text to improve===`
+    case "Explain":
+      return `If Content Safety shown - Explain why the below as it is not in line with our safety or ethical checks:\n\n===Text to reword===\n${message}\n===End of text to reword===`
+    default:
+      return "Unknown action"
+  }
+}
